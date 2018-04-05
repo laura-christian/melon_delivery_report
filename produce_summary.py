0f3618b2bb4dd249930f2df6674de27d9f45a122
Laura C <lchristian211@gmail.com>
2018-04-05 06:17:02 +0000

    Refinements to delivery_report()

--- produce_summary.py
+++ produce_summary.py
@@ -4,7 +4,7 @@ day_3 = "um-deliveries-20140521.txt"
 
 def delivery_report(file):
     """Generate report summarizing deliveries for the day"""
-    print "Day {}:\n".format(int(file[19:22])-518)
+    print "\n\nDay {}:\n".format(int(file[19:22])-518)
 
     # Open source file, parse each line into list of elements
     delivery_log = open(file)
@@ -13,12 +13,13 @@ def delivery_report(file):
         delivery_info = line.split('|')
 
     # Assign list elements to variables
-    melon_type = delivery_info[0]
-    melon_count = delivery_info[1]
-    revenue = delivery_info[2]
+        melon_type = delivery_info[0].lower()
+        melon_count = delivery_info[1]
+        revenue = delivery_info[2]
 
-    print "Delivered {} {}s for total of ${}".format(count, melon, amount)
-    the_file.close()
+        print "Delivered {} {}s for total of ${}.".format(melon_count, melon_type, revenue)
+    
+    delivery_log.close()
 
 delivery_report(day_1)
 delivery_report(day_2)
